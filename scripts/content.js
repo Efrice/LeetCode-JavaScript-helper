077b64afc89dc6f5994bf81001e0c26f29f08242
efrice <719431749@qq.com>
2023-03-19 22:54:28 +0800

    fix: genrate multiple links

--- scripts/content.js
+++ scripts/content.js
@@ -1,49 +1,54 @@
 window.addEventListener("load", () => {
-  let title = ""
-  let timer = setInterval(() => {
-    if (!document.title.includes("Loading Question...")) {
-      title = document.title.replace(/\s/g, "").split("-")[0]
-
-      fetch("https://efrice.github.io/")
-        .then((response) => response.body)
-        .then((rb) => {
-          const reader = rb.getReader()
-          return new ReadableStream({
-            start(controller) {
-              function push() {
-                reader.read().then(({ done, value }) => {
-                  if (done) {
-                    controller.close()
-                    return
-                  }
-                  controller.enqueue(value)
-                  push()
-                })
-              }
-              push()
-            },
-          })
-        })
-        .then((stream) =>
-          new Response(stream, {
-            headers: { "Content-Type": "text/html" },
-          }).text()
-        )
-        .then((result) => {
-          const solve = document.createElement("a")
-          if (result.match(title)) {
-            solve.text = "题解"
-            solve.href = "https://efrice.github.io/problems/" + title
-          } else {
-            solve.text = "暂无题解，欢迎提供"
-            solve.href = "https://efrice.github.io/"
-          }
-          solve.target = "_blank"
-          solve.style =
-            "padding-left: 20px;color:#0090a4;text-decoration: underline"
-          document.getElementsByTagName("h4")[0].append(solve)
-          clearInterval(timer)
-        })
-    }
-  }, 1e3)
+let title = "";
+const solve = document.createElement("a");
+const mainLink = "https://efrice.github.io/problems/";
+const noSolveLink = "https://efrice.github.io/";
+const solveText = "题解";
+const noSolveText = "暂无题解，欢迎提供";
+const solveStyle = "padding-left: 20px;color:#0090a4;text-decoration: underline";
+const checkTitle = () => {
+  if (!document.title.includes("Loading Question...")) {
+    title = document.title.replace(/\s/g, "").split("-")[0];
+    fetch("https://efrice.github.io/")
+      .then((response) => response.body)
+      .then((rb) => {
+        const reader = rb.getReader();
+        return new ReadableStream({
+          start(controller) {
+            function push() {
+              reader.read().then(({ done, value }) => {
+                if (done) {
+                  controller.close();
+                  return;
+                }
+                controller.enqueue(value);
+                push();
+              });
+            }
+            push();
+          },
+        });
+      })
+      .then((stream) =>
+        new Response(stream, {
+          headers: { "Content-Type": "text/html" },
+        }).text()
+      )
+      .then((result) => {
+        if (result.match(title)) {
+          solve.text = solveText;
+          solve.href = mainLink + title;
+        } else {
+          solve.text = noSolveText;
+          solve.href = noSolveLink;
+        }
+        const h4 = document.getElementsByTagName("h4")[0];
+        solve.target = "_blank";
+        solve.style = solveStyle;
+        h4.append(solve);
+        clearInterval(timer);
+      });
+  }
+};
+const timer = setInterval(checkTitle, 1000);
 })
